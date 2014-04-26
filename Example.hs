7cd89a506621114a113cada617bbcfdd25adf28b
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-04-25 23:02:39 -0500

    use st monad for ffi calls

--- Example.hs
+++ Example.hs
@@ -2,7 +2,6 @@
 
 module Main where
 
-import Foreign.C.Types
 import Data.Vector.Storable
 import Data.Vector.Eigenvalues
 
@@ -15,6 +14,5 @@ mat = fromList [
 
 main :: IO ()
 main = do
-  eigs <- eigvals 3 mat
-  print eigs
+  print $ eigvals 3 mat
 -- Just (fromList [13.348469228349522,-1.3484692283495336,-9.991844527712246e-16])
--- src/Data/Vector/Eigenvalues.hs
+++ src/Data/Vector/Eigenvalues.hs
@@ -1,28 +1,28 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-
 
 module Data.Vector.Eigenvalues (
   eigvals
 ) where
 
 import Foreign.Ptr
-import Foreign.C.Types
-import Foreign.ForeignPtr
+import Foreign.ForeignPtr (withForeignPtr, ForeignPtr)
+
+import Control.Monad.ST.Strict (runST)
+import Control.Monad.ST.Unsafe (unsafeIOToST)
 
 import qualified Data.Vector.Storable as V
 import qualified Data.Vector.Storable.Mutable as VM
 
 foreign import ccall safe "eigen.h eigen_float" eigen_float
-    :: Int -> Ptr a -> Ptr a -> IO Int
+    :: Int -> Ptr Float -> Ptr Float -> Int
 
 foreign import ccall safe "eigen.h eigen_double" eigen_double
-    :: Int -> Ptr a -> Ptr a -> IO Int
+    :: Int -> Ptr Double -> Ptr Double -> Int
+
 
 class Eigenvalues a where
-  eigvals :: Int -> a -> IO (Maybe a)
+  eigvals :: Int -> a -> Maybe a
 
 instance Eigenvalues (V.Vector Float) where
   eigvals = eigen_worker eigen_float
@@ -30,21 +30,28 @@ instance Eigenvalues (V.Vector Float) where
 instance Eigenvalues (V.Vector Double) where
   eigvals = eigen_worker eigen_double
 
+
+{-# NOINLINE vecPtr #-}
 vecPtr :: VM.Storable a => VM.MVector s a -> ForeignPtr a
 vecPtr = fst . VM.unsafeToForeignPtr0
 
 eigen_worker :: VM.Storable a
-             => (Int -> Ptr a -> Ptr a -> IO Int) -- worker function
-             -> Int                               -- dimension
-             -> V.Vector a                        -- input vector
-             -> IO (Maybe (V.Vector a))           -- output
-eigen_worker fn n vs = do
-  v <- V.thaw vs
-  out <- VM.new n
-  rc <- withForeignPtr (vecPtr v) $ \inptr -> do
-    withForeignPtr (vecPtr out) $ \outptr -> do
-      fn n inptr outptr
-  out' <- V.freeze out
-  if rc == 0
-    then return $ Just out'
-    else return Nothing
+             => (Int -> Ptr a -> Ptr a -> Int) -- worker function
+             -> Int                            -- dimension
+             -> V.Vector a                     -- input vector
+             -> Maybe (V.Vector a)             -- output
+eigen_worker fn n vs |
+  (V.length vs == n * n) = runST $ do
+    inp <- V.thaw vs
+    out <- VM.new n
+
+    rc <- unsafeIOToST $
+      withForeignPtr (vecPtr inp) $ \inptr -> do
+        withForeignPtr (vecPtr out) $ \outptr -> do
+          return $! fn n inptr outptr
+
+    out' <- V.freeze out
+    if rc == 0
+      then return $ Just out'
+      else return Nothing
+eigen_worker _ _ _ = error "dimensions are invalid"
